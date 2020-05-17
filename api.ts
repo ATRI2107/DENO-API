71b50d5b875ca59779226631e483b07615476eb9
Atri Chaturvedi <atrichaturvedi@gmail.com>
2020-05-17 10:54:07 +0530

    Api file added

---
+++ api.ts
@@ -0,0 +1,32 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+interface Course{
+    name: string,
+    price: number,
+    certification: boolean
+} 
+
+let courses: Array<Course> =[
+    {
+        name: "C++ Bootcamp",
+        price: 2.4,
+        certification: true
+    },
+    {
+        name: "MERN Bootcamp",
+        price: 2,
+        certification: true
+    },
+    {
+        name: "React Bootcamp",
+        price: 2.4,
+        certification: true
+    },
+    {
+        name: "ios 13 Bootcamp",
+        price: 0,
+        certification: false
+    }
+]
+
+const router=new Router;
